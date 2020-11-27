ee8580a67d97cfba6616602a455adc8ee50a0b10
David Audet <delzear@gmail.com>
2020-11-27 07:05:43 -0500

    Ajustments

--- nfl-card.js
+++ nfl-card.js
@@ -3,7 +3,7 @@ class NflCard extends HTMLElement {
 <style>
 .nfl-card-content{font-family:Roboto, Noto, sans-serif;font-size:14px;display: flex;flex-flow: row wrap; width:100%;}
 .nfl-card-match-container{flex: auto auto; margin:0 4px 4px 0;background: var( --ha-card-background, var(--card-background-color, white) );border-radius: var(--ha-card-border-radius, 4px);box-shadow: var( --ha-card-box-shadow, 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12) );}
-.nfl-card-match{display: flex;flex-flow: row wrap;height:70px;min-width:170px;margin:1px;padding:5px;}
+.nfl-card-match{display: flex;flex-flow: row wrap;height:70px;min-width:190px;margin:1px;padding:5px;}
 .nfl-card-teams{flex: 1 1;}
 .nfl-card-team{display: flex;flex-flow: row;white-space: nowrap;}
 .nfl-card-team-image{background-size:contain;height:35px;width:35px;}
