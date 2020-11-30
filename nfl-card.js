b8b26d790fce9419c2fbb29697ff807a677ba667
David Audet <david.audet@fivesgroup.com>
2020-11-30 09:10:47 -0500

    score width

--- nfl-card.js
+++ nfl-card.js
@@ -8,7 +8,7 @@ class NflCard extends HTMLElement {
 .nfl-card-team{display: flex;flex-flow: row;}
 .nfl-card-team-image{background-size:contain;height:35px;width:35px;}
 .nfl-card-team-name{flex: 1 auto;vertical-align:middle;line-height:35px;padding-left:5px;white-space:nowrap;overflow:hidden;}
-.nfl-card-team-score{width:60px;vertical-align:middle;line-height:35px;padding-right:5px;text-align:right;}
+.nfl-card-team-score{width:40px;vertical-align:middle;line-height:35px;padding-right:5px;text-align:right;}
 .nfl-card-quarter{width:25px;line-height:70px;vertical-align:middle;padding-right:5px;text-align:right;font-size:12px;}
 </style>
 <div class="nfl-card-content">{nfl-card}</div>
